e6f478ea7813faff03439cb4c60dde1f9e91d185
Cencious <user.email>
2022-04-18 09:39:13 +0300

    add constructors

--- src/app/class/users.ts
+++ src/app/class/users.ts
@@ -1,3 +1,4 @@
 export class Users {
-    constructor(public name:string)
+    constructor(public name:string, public login:string, public public_repos:number, public html_url:string, public created_at:Date, public followers:number, public following:number){
+    }
 }
