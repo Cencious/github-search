d7258b5d1c74528b2b1717f307c48cae96bb744e
Cencious <user.email>
2022-04-18 09:49:36 +0300

    add profile details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from './service/github.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'GitHub-Api';
+export class AppComponent implements OnInit {
+  // title = 'GitHub-Api';
+
+  profile= {
+    login:'',
+    company: '',
+    location: '',
+    avatar_url: '',
+    public_repos: '',
+    public_gist: '',
+    followers: '',
+    following: '',
+    email: '',
+    bio: '',
+    created_at: '',
+      }
+      username:string="Cencious"
+      searchText:string;
+      repos:any[]
+      users:string[]
+
+      constructor(private githubService:GithubService){}
+      ngOnInit(): void {
+        this.githubService.getData(this.username).subscribe((data)=>{this.profile=data
+        })
+
+        this.githubService.getRepos(this.username).subscribe((data) => {
+          console.log(data)
+          this.repos = data
+        })
+      }
+      searchUser(){
+        this.username = this.searchText;
+        this.ngOnInit()
+      }
 }
