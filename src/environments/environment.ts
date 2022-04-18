94a45fb837f5ca8d6fb47d82ad949776f037d5a1
Cencious <user.email>
2022-04-18 09:50:33 +0300

    add api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apikey:'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'
 };
 
 /*
