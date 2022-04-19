72ab6c6f5e267761d39187bc145697ccf6731885
Cencious <user.email>
2022-04-19 07:53:40 +0300

    add imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ColorDirective } from './directive/color.directive';
 import { UppercasePipe } from './pipe/uppercase.pipe';
 import { UsersComponent } from './components/users/users.component';
+import { DateCountPipe } from './pipe/date-count.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ColorDirective,
     UppercasePipe,
-    UsersComponent
+    UsersComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
